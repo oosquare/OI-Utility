a2543d4478523b0a8484b7d8e9a33ae47a889d12
ctj12461 <1246186412@qq.com>
2020-02-15 22:32:34 +0800

    add pointer support for FHQTreap.

--- DataStructures/Tree/FHQTreap.hpp
+++ DataStructures/Tree/FHQTreap.hpp
@@ -1,134 +1,166 @@
-template <typename T, int MaxSize>
-class FHQTreap 
-{
+template <typename T>
+class FHQTreap {
 public:
-    FHQTreap() { Seed = (int)(MaxSize * 565463ll % 2147483647); }
+    FHQTreap() { Root = nullptr; }
+
+    ~FHQTreap() {
+        if (Root)
+            delete Root;
+    }
 
     void insert(T key) {
-        int x, y;
-        split(Root, key - 1, x, y);
+        Node *x, *y;
+        split1(Root, key, x, y);
         Root = merge(merge(x, create(key)), y);
     }
 
     void remove(T key) {
-        int x, y, z;
-        split(Root, key, x, z);
-        split(x, key - 1, x, y);
-        if(y) {
-            if(Top < (MaxSize << 8) - 5) Stack[++Top] = y;
-            y = merge(Tree[y].Left, Tree[y].Right);
+        Node *x, *y, *z;
+        split2(Root, key, x, z);
+        split1(x, key, x, y);
+        if (y) {
+            y = merge(y->Left, y->Right);
         }
         Root = merge(merge(x, y), z);
     }
 
     int rank(T key) {
-        int x, y, ans;
-        split(Root, key - 1, x, y);
-        ans = Tree[x].Size + 1;
+        Node *x, *y;
+        int ans;
+        split1(Root, key, x, y);
+        ans = (x ? x->Size : 0) + 1;
         Root = merge(x, y);
         return ans;
     }
 
     T at(int r) {
-        int root = Root;
-        while (true) {
-            if (Tree[Tree[root].Left].Size + 1 == r) {
+        if (Root == nullptr || r > Root->Size)
+            return T();
+        Node *root = Root;
+        while (root) {
+            int ls = root->Left ? root->Left->Size : 0;
+            if (ls + 1 == r) {
                 break;
-            } else if (Tree[Tree[root].Left].Size + 1 > r) {
-                root = Tree[root].Left;
+            } else if (ls + 1 > r) {
+                root = root->Left;
             } else {
-                r -= Tree[Tree[root].Left].Size + 1;
-                root = Tree[root].Right;
+                r -= ls + 1;
+                root = root->Right;
             }
         }
-        return Tree[root].Key;
+        return root->Key;
     }
 
-    T prev(T key) {
-        int x, y, root;
+    T previous(T key) {
+        Node *x, *y, *root;
         T ans;
-        split(Root, key - 1, x, y);
+        split1(Root, key, x, y);
         root = x;
-        while (Tree[root].Right) root = Tree[root].Right;
-        ans = Tree[root].Key;
+        if (root == nullptr)
+            return -0x7fffffff;
+        while (root->Right) root = root->Right;
+        ans = root->Key;
         Root = merge(x, y);
         return ans;
     }
 
     T next(T key) {
-        int x, y, root;
+        Node *x, *y, *root;
         T ans;
-        split(Root, key, x, y);
+        split2(Root, key, x, y);
         root = y;
-        while (Tree[root].Left) root = Tree[root].Left;
-        ans = Tree[root].Key;
+        if (root == nullptr)
+            return 0x7fffffff;
+        while (root->Left) root = root->Left;
+        ans = root->Key;
         Root = merge(x, y);
         return ans;
     }
 
-    int size() {
-        return Tree[Root].Size;
-    }
-    
+    int size() { return (Root ? Root->Size : 0); }
+
     bool find(T key) {
-        int x, y, z;
-        split(Root, key, x, z);
-        split(x, key - 1, x, y);
+        Node *x, *y, *z;
+        split2(Root, key, x, z);
+        split1(x, key, x, y);
         bool ans;
-        if(Tree[y].Size) ans = true;
-        else ans = false;
+        if (y)
+            ans = true;
+        else
+            ans = false;
         Root = merge(merge(x, y), z);
         return ans;
     }
 
 private:
-    struct Node 
-    {
+    struct Node {
         T Key;
-        int Left, Right, Size, Priority;
-    } Tree[MaxSize];
-    int Seed, Total, Root, Top, Stack[MaxSize >> 8];
+        Node *Left, *Right;
+        int Size, Priority;
 
-    int rad() { return Seed = int(Seed * 482711ll % 2147483647); }
+        Node(T key, int pri) : Key(key), Left(nullptr), Right(nullptr), Size(1), Priority(pri) {}
+        ~Node() {
+            if (Left)
+                delete Left;
+            if (Right)
+                delete Right;
+        }
+    } * Root;
+    int Seed;
 
-    int create(T key) {
-        int root = Top ? Stack[Top--] : ++Total;
-        Tree[root].Key = key;
-        Tree[root].Size = 1;
-        Tree[root].Left = Tree[root].Right = 0;
-        Tree[root].Priority = rad();
+    int random() { return Seed = int((Seed + 3) * 482711ll % 2147483647); }
+
+    Node *create(T key) {
+        Node *root = new Node(key, random());
         return root;
     }
 
-    void pushup(int root) {
-        if(root != 0)
-            Tree[root].Size = Tree[Tree[root].Left].Size + Tree[Tree[root].Right].Size + 1; 
+    void pushup(Node *root) {
+        if (root)
+            root->Size = (root->Left ? root->Left->Size : 0) + (root->Right ? root->Right->Size : 0) + 1;
     }
 
-    void split(int root, T key, int &x, int &y) {
-        if (root == 0) {
-            x = y = 0;
+    void split1(Node *root, T key, Node *&x, Node *&y) {
+        if (root == nullptr) {
+            x = y = nullptr;
             return;
         }
-        if (!(key < Tree[root].Key)) {
+        if (root->Key < key) {
             x = root;
-            split(Tree[root].Right, key, Tree[root].Right, y);
+            split1(root->Right, key, root->Right, y);
         } else {
             y = root;
-            split(Tree[root].Left, key, x, Tree[root].Left);
+            split1(root->Left, key, x, root->Left);
         }
         pushup(root);
     }
 
-    int merge(int x, int y) {
-        if (x == 0 || y == 0)
-            return x + y;
-        if (Tree[x].Priority > Tree[y].Priority) {
-            Tree[x].Right = merge(Tree[x].Right, y);
+    void split2(Node *root, T key, Node *&x, Node *&y) {
+        if (root == nullptr) {
+            x = y = nullptr;
+            return;
+        }
+        if (!(key < root->Key)) {
+            x = root;
+            split2(root->Right, key, root->Right, y);
+        } else {
+            y = root;
+            split2(root->Left, key, x, root->Left);
+        }
+        pushup(root);
+    }
+
+    Node *merge(Node *x, Node *y) {
+        if (x == nullptr)
+            return y;
+        if (y == nullptr)
+            return x;
+        if (x->Priority > y->Priority) {
+            x->Right = merge(x->Right, y);
             pushup(x);
             return x;
         } else {
-            Tree[y].Left = merge(x, Tree[y].Left);
+            y->Left = merge(x, y->Left);
             pushup(y);
             return y;
         }
